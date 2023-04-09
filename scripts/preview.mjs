cbdd0a8b1c29913b417dafdb87394fe5cb3b6513
许圣峰 <kricsleo@163.com>
2023-04-09 20:50:42 +0800

    chore: preview script

--- scripts/preview.mjs
+++ scripts/preview.mjs
@@ -1,8 +1,9 @@
 #!/usr/bin/env zx
-import 'zx/globals'
+
 const { name, version } = await fs.readJson('./package.json')
 const vsixPath = `${name}-${version}.vsix`
 
+await $`npm run build`
 await $`code --uninstall-extension kricsleo.gentle-clean`
 await $`code --install-extension ${vsixPath}`
 
