f4e94fc9116b5269d0cf2216771176c0e5dfec45
许圣峰 <kricsleo@163.com>
2023-04-10 18:34:34 +0800

    chore: preview script

--- scripts/preview.mjs
+++ scripts/preview.mjs
@@ -2,9 +2,10 @@
 
 const { name, version } = await fs.readJson('./package.json')
 const vsixPath = `${name}-${version}.vsix`
+const extensionId = 'kricsleo.gentle-clean'
 
 await $`npm run build`
-await $`code --uninstall-extension kricsleo.gentle-clean`
+await $`code --uninstall-extension ${extensionId}`
 await $`code --install-extension ${vsixPath}`
 
 console.log(chalk.yellow('Installed'),  '🫧 ', vsixPath)
